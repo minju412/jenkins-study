53547e15fde57ef11a78e7a5ab4b6a7b4d5d5509
minju412 <ln8847@ajou.ac.kr>
2024-01-23 00:44:03 +0900

    feat: Update HomeController

--- src/main/java/org/example/jenkins_study/HomeController.java
+++ src/main/java/org/example/jenkins_study/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping()
     public String home() {
-        return "home33";
+        return "home44";
     }
 }
